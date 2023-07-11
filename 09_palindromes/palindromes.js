a1e9509c901ff90a8c1541118975a2f90c7e6050
John Fourchy <fourchy151@gmail.com>
2023-07-10 22:03:57 -0700

    Add 09_palindrome solution

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,19 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    str = str.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g,"").toLowerCase().split(" ").join("");
+    let s = 0;
+    let e = str.length - 1;
 
+    while(s < e) {
+        //console.log(str.charAt(s) + " " + str.charAt(e));
+        if(str.charAt(s) == str.charAt(e)) {
+            s += 1;
+            e -= 1;
+        }
+        else {
+            return false;
+        }
+    }
+    return true;
 };
 
 // Do not edit below this line
