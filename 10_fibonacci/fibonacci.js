088ca795517ee9eb33c31231d39b178497c93dd7
John Fourchy <fourchy151@gmail.com>
2023-07-11 20:34:23 -0700

    Add 10_fibonacci solution

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,18 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    if(num < 0) {
+        return "OOPS";
+    }
 
+
+    let fnum = 1;
+    let snum = 1;
+    let t = 1;
+    for(let i = 0; i < num - 2; i++) {
+        t = fnum + snum;
+        fnum = snum;
+        snum = t;
+    }
+    return t;
 };
 
 // Do not edit below this line
